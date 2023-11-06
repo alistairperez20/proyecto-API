6039e992d9c438ffdcd984818f1a6b183095f834
iVale2002 <irlanquiroz492@gmail.com>
2023-11-06 12:08:02 -0600

    codigo  Weather controller

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,2 +1,18 @@
+require 'httparty'
+
 class WeatherController < ApplicationController
+  def index
+    response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=YourCity&appid=YourAPIKey")
+    data = JSON.parse(response.body)
+
+    if response.code == 200
+      temperature = data['main']['temp']
+      description = data['weather'][0]['description']
+      city = data['name']
+
+      WeatherData.create(temperature: temperature, description: description, city: city)
+    else
+      flash[:error] = 'Error al obtener datos del clima'
+    end
+  end
 end
