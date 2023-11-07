7d3ab1d776b5081cda49ff075c8f995495c0c8ca
alistairperez20 <ajpl.090@gmail.com>
2023-11-07 14:00:53 -0600

    agregar codigo a weather controller

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,18 +1,41 @@
-require 'httparty'
-
+# app/controllers/weather_controller.rb
 class WeatherController < ApplicationController
+  require 'httparty'
+
   def index
-    response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=YourCity&appid=433eb2c0cc8d4e79bc9f87cefd0f19bb")
-    data = JSON.parse(response.body)
+    @weather_data = nil
+  end
+
+  def fetch_weather
+    api_key = '433eb2c0cc8d4e79bc9f87cefd0f19bb'
+    city = params[:city]
+
+    response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{api_key}")
 
     if response.code == 200
-      temperature = data['main']['temp']
-      description = data['weather'][0]['description']
-      city = data['name']
-
-      WeatherData.create(temperature: temperature, description: description, city: city)
+      @weather_data = JSON.parse(response.body)
     else
       flash[:error] = 'Error al obtener datos del clima'
     end
+
+    render 'index'
+  end
+
+  def create
+    @weather_data = WeatherData.new(weather_data_params)
+
+    if @weather_data.save
+      flash[:success] = 'Datos del clima guardados exitosamente'
+    else
+      flash[:error] = 'Error al guardar los datos del clima en la base de datos'
+    end
+
+    redirect_to weather_index_path
+  end
+
+  private
+
+  def weather_data_params
+    params.require(:weather_data).permit(:city, :temperature, :description)
   end
 end
