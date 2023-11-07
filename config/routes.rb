8077ffd44d9cdea972a95a0f9df4d1dcf777ea7b
alistairperez20 <ajpl.090@gmail.com>
2023-11-07 15:03:35 -0600

    se configura el index como root en la ruta rpincipal

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # config/routes.rb
-get '/weather', to: 'weather#index', as: 'weather_index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  #get '/weather', to: 'weather#index', as: 'weather_index'
+
+  # Define la acción 'index' de 'WeatherController' como la página principal
+  root 'weather#index'
 end
